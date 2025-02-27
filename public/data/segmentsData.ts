9861923bb29d01dd463ad97fac55de029001fb05
dizzydwarfus <lianzhenyang1998@gmail.com>
2025-02-27 19:17:01 +0100

    added public/data to repo

---
+++ public/data/segmentsData.ts
@@ -0,0 +1,183 @@
+export const icebergSegments = [
+  {
+    id: "L1",
+    title: "Professional Overview",
+    image: "/images/iceberg_L1.jpg",
+    width: 1000,
+    height: 300,
+    description: `A broad look at my professional life.`,
+    resumeKey: "experience",
+  },
+  {
+    id: "L2",
+    title: "Personal Projects",
+    image: "/images/iceberg_L2.jpg",
+    width: 1000,
+    height: 200,
+    description: `Projects I've built in my spare time.`,
+    resumeKey: "projects",
+  },
+  {
+    id: "L3",
+    title: "Personal Growth & Learnings",
+    image: "/images/iceberg_L3.jpg",
+    width: 1000,
+    height: 200,
+    description: "Key milestones and lessons learned along my journey.",
+    details: {
+      milestones: [
+        {
+          year: "2017 - 2020",
+          title: "Foundations in Chemical Engineering",
+          color: "teal",
+          lessons: [
+            "Laid the groundwork in process design and reaction engineering.",
+            "Discovered an interest in numerical methods through lab research.",
+            "Cultivated project discipline via the Honours Programme.",
+          ],
+        },
+        {
+          year: "2019",
+          title: "International Summer School Experience",
+          color: "teal",
+          lessons: [
+            "Explored the social impact of Barcelona’s Superilles (urban planning).",
+            "Gained cultural perspective and collaborative research skills.",
+          ],
+        },
+        {
+          year: "2020 - 2022",
+          title: "Immersing in Advanced Process & Python",
+          color: "teal",
+          lessons: [
+            "Mastered Python for a Master’s thesis on catalytic reactions in fractal spaces.",
+            "Strengthened a data-driven mindset for approaching engineering problems.",
+            "Learned to pivot from purely theoretical models to real-world process optimization.",
+          ],
+        },
+        {
+          year: "2022",
+          title: "Operational Efficiency Internship",
+          color: "teal",
+          lessons: [
+            "Applied data analysis to real-world labs at Eurofins, boosting profitability.",
+            "Developed a project tracker and budget workflows, learning the power of data transparency.",
+            "Realized the value of bridging technical solutions with business outcomes.",
+          ],
+        },
+        {
+          year: "2023",
+          title: "Scaling Dev & Data Pipelines",
+          color: "teal",
+          lessons: [
+            "Automated daily production KPIs and advanced Python ETL at BASF, saving time and money.",
+            "Saw firsthand how data insights drive operational changes (OEE improvements, waste reduction).",
+            "Blended engineering fundamentals with data science for bigger impacts.",
+          ],
+        },
+        {
+          year: "2024",
+          title: "Platform Engineer & DevOps Mindset",
+          color: "teal",
+          lessons: [
+            "Embraced Kubernetes, Terraform, and Helm for scalable platform solutions.",
+            "Began addressing alert fatigue and certificate management as pivotal devops problems.",
+            "Solidified a deeper understanding of the synergy between software and infrastructure.",
+          ],
+        },
+        {
+          year: "2025",
+          title: "Growing as Platform Engineer",
+          color: "teal",
+          lessons: [
+            "Developing expertise in AWS Core Services, Kubernetes, Terraform and Helm for managing infrastructure.",
+            "Becoming comfortable with platform observability stack - datadog, prometheus, pagerduty",
+            "Innersourcing within the company has allowed me to adopt proficiency in new programming languages - Javascript/Typescript - mainly react (frontend) and express/next (backend).",
+          ],
+        },
+      ],
+    },
+  },
+  {
+    id: "L4",
+    title: "Hobbies & Interests",
+    image: "/images/iceberg_L4.jpg",
+    width: 1000,
+    height: 200,
+    description: "A collection of activities that spark my joy and curiosity.",
+    details: {
+      hobbies: [
+        {
+          name: "Guitar",
+          icon: "/images/hobbies/guitar.png",
+          level: "Casual Player",
+          description:
+            "I love learning acoustic fingerstyles and chord progressions on songs that I enjoy listening/singing to.",
+        },
+        {
+          name: "Analyzing Companies",
+          icon: "/images/hobbies/financial_statement.png",
+          level: "Enthusiast",
+          description:
+            "Following the economy and studying financial statements to learn how a company is valued on paper.",
+        },
+        {
+          name: "Basketball",
+          icon: "/images/hobbies/basketball.jpg",
+          level: "Favourite Sport",
+          description:
+            "A stress reliever by myself or a fun activity with a group.",
+        },
+        {
+          name: "Indoor Bouldering & Climbing",
+          icon: "/images/hobbies/indoor_climbing.png",
+          level: "Weekly Climber",
+          description:
+            "Great physical challenge and problem-solving on the wall—improves focus and strength.",
+        },
+      ],
+    },
+  },
+  {
+    id: "L5",
+    title: "Core Values and Principles",
+    image: "/images/iceberg_L5.jpg",
+    width: 1000,
+    height: 200,
+    description: "The guiding principles that shape who I am.",
+    details: {
+      values: [
+        {
+          name: "Continuous Learning",
+          icon: "/images/values/learning.png",
+          description:
+            "I believe we are students for life. There is always something new to learn, explore, and master. Learning is not just about skill acquisition but about expanding my worldview.",
+        },
+        {
+          name: "Ownership",
+          icon: "/images/values/ownership.png",
+          description:
+            "I take responsibility for my tasks, challenges, and decisions. Whether in success or failure, I step up and take action rather than waiting for someone else to fix the problem.",
+        },
+        {
+          name: "Resilience",
+          icon: "/images/values/resilience.png",
+          description:
+            "Setbacks and failures do not define me—they drive me forward. I tackle obstacles head-on, adapting and pushing through difficulties without losing sight of my goals.",
+        },
+        {
+          name: "Curiosity",
+          icon: "/images/values/curiosity.png",
+          description:
+            "I have a deep fascination for understanding how things work across various fields—finance, economics, psychology, sports, technology, natural sciences, law, and more. My curiosity fuels my desire to connect ideas across disciplines.",
+        },
+        {
+          name: "Empathy",
+          icon: "/images/values/empathy.png",
+          description:
+            "Everyone's perspectives are shaped by their unique experiences. I seek to understand before judging, valuing different viewpoints and learning from the perspectives of others.",
+        },
+      ],
+    },
+  },
+];
