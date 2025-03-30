3fd486cb53dc41dacc71622b233e9d1a8bf38145
dizzydwarfus <lianzhenyang1998@gmail.com>
2025-03-30 18:34:05 +0200

    added caching headers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: "standalone",
+  images: {
+    minimumCacheTTL: 86400, // 1 day
+  },
+  async headers() {
+    return [
+      {
+        // Apply cache-control headers for all files in the /images folder
+        source: "/images/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        // Apply cache-control headers for all files in the /data folder
+        source: "/data/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
